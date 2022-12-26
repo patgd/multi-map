7dca5f5c910c45e2dedb4dd0c1e74f755f3c94a6
greytrick <patdg@pm.me>
2022-12-26 10:22:45 -1000

    Initial Commit

---
+++ MultiMap/MultiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapApp.swift
+//  MultiMap
+//
+//  Created by pat on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
