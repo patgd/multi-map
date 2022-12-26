f538de7ec9fafd5c2642ef75b0932181b3689aaf
greytrick <patdg@pm.me>
2022-12-26 12:38:48 -1000

    Added a map and two annotations.

--- MultiMap/ContentView.swift
+++ MultiMap/ContentView.swift
@@ -6,14 +6,28 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 21.494451, longitude: -157.943498), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    let locations = [
+            Location(name: "Lanis", latitude: 21.618863, longitude: -158.088967),
+            Location(name: "Diamond Head", latitude: 21.253925, longitude: -157.805288)
+        ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Map(coordinateRegion: $region, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    Text(location.name)
+                        .font(.headline)
+                        .padding(5)
+                        .padding(.horizontal, 5)
+                        .background(.black)
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+            }
         }
         .padding()
     }
---
+++ MultiMap/Location.swift
@@ -0,0 +1,31 @@
+//
+//  Location.swift
+//  MultiMap
+//
+//  Created by pat on 12/26/22.
+//
+
+import MapKit
+
+struct Location: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
